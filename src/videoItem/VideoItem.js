a504c22f3d6e89128544566e12a80e5c3e1f9fe7
Hila El Peleg <131517121+hilaelpeleg@users.noreply.github.com>
2024-06-08 14:50:39 +0300

    merge register to main (#2)
    * merge register to main
    
    * Home page - create the Designing the page + adding a Jason file of the videos
    
    * design the video

--- src/videoItem/VideoItem.js
+++ src/videoItem/VideoItem.js
@@ -1,6 +1,26 @@
-function VideoItem(){
-    return(
-        <div>hello</div>
+import '../app/profile.css'
+
+
+function VideoItem({ thumbnailUrl, title, uploader, description, likes, uploadDate, duration }) {
+    return (
+        <div >
+            <div className="thumbnail-container">
+            <img src={thumbnailUrl} class="card-img-top" alt="profile" />
+            <div className="square" >
+                {duration}
+            </div>
+            </div>
+            
+            <div class="card-body">
+                <img id="profile" src="noa.jpg" />
+                <h5 className="card-text title">{title}</h5>
+                <div className="box">
+                <p className="card-text p">{uploader}</p>
+                <p className="card-text p">{description}</p>
+                <p class="card-text"><small class="text-body-secondary">{uploadDate} { likes}</small></p>
+                </div>
+            </div>
+        </div>
     );
 }
 
---
+++ src/videoItem/VideoItems.js
@@ -0,0 +1,32 @@
+import VideoItem from "./VideoItem";
+import React, { useEffect } from 'react';
+import videosData from '../app/videos.json';
+import './videoItem.css';
+
+function VideoItems({videoList, setVideoList}) {
+    useEffect(() => {
+        // Set the video list when the component mounts
+        setVideoList(videosData);
+    }, [setVideoList]);
+
+
+    return (
+        <div className="row gx-3">
+            {videoList.map(video => (
+                <div key={video.id} className="col-xl-4 col-lg-4 col-md-4 col-sm-12 hover"> {/* Adjust col classes to occupy 4 columns for xl, lg, and md screens */}
+                    <VideoItem
+                        thumbnailUrl={video.thumbnailUrl}
+                        title={video.title}
+                        uploader={video.uploader}
+                        description={video.description}
+                        likes={video.likes}
+                        uploadDate={video.uploadDate}
+                        duration={video.duration}
+                    />
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default VideoItems;
