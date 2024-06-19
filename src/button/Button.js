9b45299e48211b84ec395d27308bf36902ff76eb
hilaelpeleg <183hila@gmail.com>
2024-06-19 22:19:52 +0300

    Changing the registration and login button on the registration page.

--- src/button/Button.js
+++ src/button/Button.js
@@ -1,14 +1,13 @@
 import './button.css'
 
 
-function Button({onClick, value}) {
+function Button({ onClick, value }) {
 
 
     return (
         <div className="d-grid col-6 mx-auto my-3 widthb">
-            <button className="btn btn-primary" type="button" onClick={onClick}>{value} </button>
+            <button className="btn btn-primary color" type="button" onClick={onClick}>{value} </button>
         </div>
-        
     )
         ;
 }
