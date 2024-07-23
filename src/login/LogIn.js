d74d7d4fdd5bc2c627309b8fa18dda3126c0f1cd
hilaelpeleg <183hila@gmail.com>
2024-07-23 23:57:17 +0300

    fix errors login

--- src/login/LogIn.js
+++ src/login/LogIn.js
@@ -10,12 +10,15 @@ function LogIn({ userList, setUserLogin }) {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const [formErrors, setFormErrors] = useState({});
 
     const navigate = useNavigate();
 
         // Function to handle login logic
 
     const handleLogin = () => {
+        setFormErrors(validateLogin(userName, password));
+
                 // Find the user in the userList with the provided username and password
 
         const user = userList.find(user => user.userName === userName && user.password === password);
@@ -31,16 +34,27 @@ function LogIn({ userList, setUserLogin }) {
         }
     };
 
+    const validateLogin = (userName, password) => {
+        const errors = {};
+        if (!userName) {
+            errors.userName = "User name is required!";
+        }
+        if (!password) {
+            errors.password = "Password is required!";
+        }
+        return errors;
+    };
+
     return (
         <div className="wrapper">
         <div className="card custom-card-width-L container" >
             <div className="card-body">
             <h5 className="card-title">Log In</h5>
                 <TextInputLog className="form-control" name="userName" kind="user name"
-                    value={userName} onChange={(event) => setUserName(event.target.value)} />
+                    value={userName} onChange={(event) => setUserName(event.target.value)}  error={formErrors.userName}/>
                 <div className="row">
                     <TextInputLog name="password" kind="password" type="password"
-                        value={password} onChange={(event) => setPassword(event.target.value)} />
+                        value={password} onChange={(event) => setPassword(event.target.value)}  error={formErrors.password}/>
                 </div>
             </div>
             <div className="list-group list-group-flush">
