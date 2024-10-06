1f54d8a85e55e0d35b883734f2a551816f8c3864
hilaelpeleg <183hila@gmail.com>
2024-10-06 16:41:35 +0300

    Decomposing the user page into components

--- src/user/UserAccount.js
+++ src/user/UserAccount.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import LeftMenu from '../homePage/LeftMenu'; // Import the LeftMenu component
 import './UserAccount.css';
-import { API_URL } from '../config';
-
-
+import UserProfileCard from './UserProfileCard';
+import UserPageMenu from './UserPageMenu';
 
 const UserAccount = ({ darkMode, setDarkMode, logedinuser, videoList, setVideoList }) => {
   const user = logedinuser ? logedinuser : null;
@@ -18,89 +17,18 @@ const UserAccount = ({ darkMode, setDarkMode, logedinuser, videoList, setVideoLi
         {/* Main Content */}
         <div className="col-12">
           <div className="d-flex justify-content-end align-items-center mb-4">
-            {/* Profile Picture */}
-            <div className="me-3">
-              <div className="cardUserA" >
-                <div className="card-body">
-                  <div>
-                    <div className="profile-container">
-                      <div className="circular-profile">
-                        <img src={user && user.profilePicture ? `${API_URL}${user.profilePicture}` : 'path-to-default-image'} alt="Profile" />
-                      </div>
-                      <div className="user-info">
-                        {/* שם המשתמש */}
-                        <h1 className="user-username">{user.userName}</h1>
-                        {/* שם פרטי ושם משפחה מתחת לשם המשתמש */}
-                        <p className="user-fullname">
-                          {user.firstName} {user.lastName}
-                        </p>
-
-
-
-                        <div className="row-buttons-container">
-            <div className='btn-group-container-row'>
-                <button type="button" className="btn btn-light margin">
-                    {/* <img className="marginbutton" src={share} alt="Share" /> */}
-                    Share
-                </button>
-                <button type="button" className="btn btn-light margin">
-                    {/* <img className="marginbutton" src={download} alt="Download" /> */}
-                    Download
-                </button>
-                            </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Profile Info */}
+          <UserProfileCard user={user} />
           </div>
           {/* Navigation Bar */}
-          <ul className="nav nav-tabs mb-4">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Playlists
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Community
-              </a>
-            </li>
-          </ul>
-
+          <UserPageMenu />
           {/* Content Area */}
           <div className="text-center">
             <div className="mb-4">
-              <img src="path-to-image" alt="Illustration" width="200" />
+              {/* Videos */}
             </div>
             <p className="lead">
               Create content from anywhere. Upload and record at home or from anywhere, and everything you create will appear here.
             </p>
-            <button className="btn btn-dark">Create</button>
           </div>
         </div>
       </div>
---
+++ src/user/UserProfileCard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { API_URL } from '../config';
+import subscribe from '../homePage/svg icons/subscribe.svg';
+
+const UserProfileCard = ({ user }) => {
+  return (
+    <div className="menu-navigation">
+      <div className="me-3">
+        <div className="cardUserA" >
+          <div className="card-body">
+            <div>
+              <div className="profile-container">
+                <div className="circular-profile">
+                  {/* Display profile picture, or default if not available */}
+                  <img src={user && user.profilePicture ? `${API_URL}${user.profilePicture}` : 'path-to-default-image'} alt="Profile" />
+                </div>
+                <div className="user-info userSub">
+                  {/* Display username */}
+                  <h1 className="user-username">{user.userName}</h1>
+                  {/* Display first and last name */}
+                  <p className="user-fullname">
+                    {user.firstName} {user.lastName}
+                  </p>
+                  {/* Subscribe button */}
+                  <div className="row-buttons-container marginupSUb">
+                    <div className='btn-group-container-row widthSub'>
+                      <button type="button" className="btn btn-light margin marginSub">
+                        <img className="marginbutton" src={subscribe} alt="subscribe" />
+                        Subscribe
+                      </button>
+                    </div> 
+                  </div> 
+                </div> 
+              </div> 
+            </div> 
+          </div> 
+        </div> 
+      </div>
+    </div>
+  );
+};
+
+export default UserProfileCard;
